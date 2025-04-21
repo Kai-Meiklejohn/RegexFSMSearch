9904baaeb0660233bde8fb20cb77d3797abef083
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2025-04-21 21:23:00 +1200

    quick notes

--- REcompile.java
+++ REcompile.java
@@ -4,6 +4,19 @@
 
 public class REcompile {
     public static void main(String[] args) {
-        
+        // TODO
+        // Notes: make sure it takes in valid arguments
+        // if something like a** is provided, it should still let it passed in but make it a*
+        // but obviously something like a++ is not allwoed
+
+        // our expressions allow +(or), *(0 to many)
+        // for eg ab means a then b
+        // a* means 0 or more a's
+        // a+b means a or b
+        // a+(bc) a or bc
+        // a+(bc)* a or bc 0 or more times
+
+        //implement the code t hat tony made in the lecture (the one that he ssays is C like)
+        // write the sudo code that tony gave us first
     }
 }
